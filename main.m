2d37ad8665b683185eb213bc835d0bd781962b7e
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2016-10-30 16:17:11 -0400

    fixes

--- main.m
+++ main.m
@@ -5,32 +5,38 @@
 % 
 % Oleksandr Kazakov/ UAlbany Physics Department date created 9/12/2011
 
-load('data.mat'); % input your data in that file
+% input your data in that file
+load('data.mat'); 
 
-b=(c1/c2-1)*c2/(c1*h); % Normalization constants for Interdiffusion
-g=2*(1/(1-c11)+1/c22)*(c22-c11); %Normalization constants for Volume Diffusion
+% Normalization constants
+% for Interdiffusion:
+b=(c1/c2-1)*c2/(c1*h); 
+% for Volume Diffusion
+g=2*(1/(1-c11)+1/c22)*(c22-c11);
 
-
-[Dopt] = DopT( xi,ti,c1,c2,h); % optimal value of Diffusion Coefficient
-[pogr] = pogR( xi,ti,Dopt,b,s);
-[Dvol] = DvoL( x1i,t1i,c11,c22,h);
-[pogrv] = pogrV( x1i,t1i,Dvol,g,s);
+% Optimal value of Diffusion Coefficient
+[Dopt] = DopT(xi,ti,c1,c2,h); 
+[pogr] = pogR(xi,ti,Dopt,b,s);
+[Dvol] = DvoL(x1i,t1i,c11,c22,h);
+[pogrv] = pogrV(x1i,t1i,Dvol,g,s);
 
 display(['Probable value of interdiffusion D = ' num2str(Dopt) ' +- ' num2str(pogr)]);
 display(['Probable value of volume D = ' num2str(Dvol) ' +- ' num2str(pogrv)]);
 
-M=0:1:100;loggg=zeros(1,length(M));
-% - I am are creating array of 1XR with zero elements
+M=0:1:100;
+loggg=zeros(1,length(M));
+
 for u=1:length(M) 
     loggg(u)=logP(xi,ti,M(u),s); 
 end
-% - in cycle I am adding a values of R; by guesing its range
 figure;
 plot(M,loggg); % Logposterior probability
-c = fminsearch(@(M)logP(xi,ti,M,s),0); % Defining minimum of LogP function
 
+% Defining minimum of LogP function
+c = fminsearch(@(M)logP(xi,ti,M,s),0); 
 
-Dprob=(c/b)*1e-6; % correction 1e-6sm is due to the micron scale 
+% Correction 1e-6sm is due to the micron scale 
+Dprob=(c/b)*1e-6; 
 
 display(Dprob);
 
@@ -50,7 +56,7 @@ Dvol=(prob/g)*1e-6;
 
 figure; 
 
-%Now we plot it to see how good are results are
+% Now we plot actual vs fitted results
 t = 0:0.5:16;
 x = sqrt(b*(Dopt/1e-9)*t);
 x1 = sqrt(b*((Dopt+pogr)/1e-9)*t);
@@ -72,8 +78,6 @@ plot(t0,x0,t0,x11,'r',t0,x22,'r');
 xlabel('t, hours')
 ylabel('x^2, 10^(-7),m')
 title('Plot of the phase grows kinetics. Front layer width over the time. Volume','FontSize',12)
-
-% The End;
 
 
 
